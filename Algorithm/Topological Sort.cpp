ee52b60b5cf9cae70801a8d58525c13ad345d883
hjkim <guswns589898@gmail.com>
2022-09-27 15:14:05 +0900

    Add push, other method source code

--- Algorithm/Topological Sort.cpp
+++ Algorithm/Topological Sort.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include <algorithm>
 #include <vector>
 using namespace std;
@@ -58,4 +58,86 @@ int main() {
 
 	topologicalSort();
 	printOrder();
+}
+*/
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+#define NODECOUNT 7				// 노드의 갯수
+
+int inDegree[NODECOUNT + 1];		// 진입 차수 
+
+vector<int> Nodes[NODECOUNT + 1]; // 모든 Node의 Edge 정보
+
+void topologySort() {
+	int sortedNode[NODECOUNT + 1];// 위상정렬을 수행한 Node 순서 결과
+	queue<int> Q;
+
+	// 진입 차수가 0인 Node를 Queue에 모두 추가
+	for (int i = 1; i <= NODECOUNT; i++) {
+		if (inDegree[i] == 0) {
+			Q.push(i);
+		}
+	}
+
+	// 모든 Node 방문하여 간선이 없을 때까지 순서화된 Node 결과를 얻음
+	for (int i = 1; i <= NODECOUNT; i++) {
+		// N개의 모든 노드를 방문하기 전에 Queue가 empty이면 cycle이 발생함을 의미
+		if (Q.empty()) {
+			cout << "there may be a cycle node" << endl;
+			return;
+		}
+
+		int x = Q.front();
+		Q.pop();
+		sortedNode[i] = x;
+
+		// 연결이 되어있는 모든 정점들을 확인하면서 간선제거 
+		for (int j = 0; j < Nodes[x].size(); j++) {
+			int y = Nodes[x][j];
+
+			// 진입차수가 '0'인 노드 큐에 추가
+			if (--inDegree[y] == 0) {
+				Q.push(y);
+			}
+		}
+	}
+
+	for (int i = 1; i <= NODECOUNT; i++) {
+		cout << "Ordered Node:" << sortedNode[i] << endl;
+
+		Nodes[i].clear();
+	}
+
+}
+
+void addEdge(int start, int end)
+{
+	// Edge 연결, 방향성을 갖음
+	Nodes[start].push_back(end);
+
+	// 진입 차수 증가
+	inDegree[end]++;
+}
+
+void solve()
+{
+	addEdge(1, 2);
+	addEdge(3, 2);
+	addEdge(3, 5);
+	addEdge(2, 4);
+	addEdge(4, 6);
+	addEdge(5, 6);
+	addEdge(6, 7);
+
+	topologySort();
+}
+
+int main()
+{
+	solve();
+	return 0;
 }
