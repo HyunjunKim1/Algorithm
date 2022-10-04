962655538d3e306f3b62588ed71615d887c79c63
hjkim <guswns589898@gmail.com>
2022-10-04 09:24:28 +0900

    Selection modify

--- Algorithm/Insertion Sort.cpp
+++ Algorithm/Insertion Sort.cpp
@@ -4,13 +4,12 @@ using namespace std;
 
 int main() {
 
-	int arr[] = { 3, 4, 2, 5, 1 };
-	int arrSize = 5;
+	int arr[] = { 26, 54, 93, 17, 77, 31, 44, 55, 20 };
+	int arrSize = 9;
 
-	//배열의 시작점부터 끝까지 탐색한다.
-	for (int i = 0; i < arrSize - 1; i++) {
-		//배열의 두 번째 원소부터, 해당 원소의 왼쪽에 있는 값들을 비교하고, 원소보다 작은 값을
-		//만나면 그 뒤에 해당 원소를 삽입한다.
+	for (int i = 0; i < arrSize - 1; i++) 
+	{
+	
 		for (int j = i + 1; j > 0 && arr[j - 1] > arr[j]; j--) {
 			int temp = arr[j - 1];
 			arr[j - 1] = arr[j];
--- Algorithm/Selection Sort.cpp
+++ Algorithm/Selection Sort.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 using namespace std;
 
-//초기값을 배열에서 나올 수 없는 큰 수로 초기화하여 배열 내의 초기값을 찾는다.
 #define INF 987654321
 
 int main() {
 
-	int arr[5] = { 3,1,5,2,4 };
-	int arrSize = 5;
+	int arr[] = { 26, 54, 93, 17, 77, 31, 44, 55, 20 };
+	int arrSize = 9;
 	for (int i = 0; i < arrSize; i++) {
 		int min_num = INF; 
 		int temp, min_index;
